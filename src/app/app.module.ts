3ebee9c46ed9d7755c442d18a7458fd0cf529561
bagheri2022 <106174798+bagheri2022@users.noreply.github.com>
2022-06-13 18:15:53 +0430

    14010323

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatSliderModule} from '@angular/material/slider';
-
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+const routes: Routes = [
+  { path: 'home', component:HomeComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+     [RouterModule.forRoot(routes)],
+     MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
